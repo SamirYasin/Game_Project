dcadf14fb454c9a4638e95b085e6e22cc30b0fe1
Dajve Echols <dajvelechols@gmail.com>
2023-04-08 13:57:06 -0500

    added proper logic for user to choose game

--- src/Interface/MainInterface.java
+++ src/Interface/MainInterface.java
@@ -2,24 +2,19 @@ package Interface;
 
 import tic_tac_toe.GameBoard;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MainInterface {
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome");
-        System.out.println("Choose A for: Tic tac toe");
-        System.out.println("Choose B for: Bank");
-        System.out.println("Choose C for: HandMan");
-        System.out.println("Choose D for: Random number memory game");
-        System.out.println("Choose E for: Movie API");
 
-        String usersChoice = scanner.next();
-        userOptions(usersChoice);
+    private static final String[] availableServices = {"A"};
+
+    public void run() {
+        System.out.println("Welcome");
+        takeUserToGame(promptUserForService());
     }
 
-    private void userOptions(String usersChoice) {
-
+    private void takeUserToGame(String usersChoice) {
         String choice = usersChoice.toUpperCase();
 
         switch (choice) {
@@ -32,4 +27,50 @@ public class MainInterface {
                 break;
         }
     }
+
+    private String promptUserForService() {
+        Scanner scanner = new Scanner(System.in);
+        boolean breakOutLoop = false;
+        String usersChoice;
+
+        do {
+            System.out.println("Choose A for: Tic tac toe");
+            System.out.println("Choose B for: Bank (Service currently not available)");
+            System.out.println("Choose C for: HandMan (Service currently not available)");
+            System.out.println("Choose D for: Random number memory game (Service currently not available)");
+            System.out.println("Choose E for: Movie API (Service currently not available)");
+
+            usersChoice = scanner.next();
+            boolean isChoiceASingleLetter = isUsersChoiceALetter(usersChoice);
+            boolean isChoiceAService = isServiceAvailable(usersChoice);
+
+            if (!isChoiceASingleLetter) {
+                System.out.println("Please choose either A, B, C, D, or E");
+                continue;
+            }
+
+            if (!isChoiceAService) {
+                System.out.println("Sorry this service is currently unavailable");
+                continue;
+            }
+
+            breakOutLoop = true;
+
+        } while (!breakOutLoop);
+
+        return usersChoice;
+    }
+
+    private boolean isServiceAvailable(String usersChoice) {
+        for (String service : availableServices) {
+            if (Objects.equals(usersChoice.toUpperCase(), service)) return true;
+        }
+        return false;
+    }
+
+    private boolean isUsersChoiceALetter(String usersChoice) {
+        if (usersChoice.length() != 1) return false;
+        return Character.isLetter(usersChoice.charAt(0));
+    }
 }
+
