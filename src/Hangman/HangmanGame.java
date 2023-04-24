fea44edf7d80c2879be0de46f25609a9da2a1106
sammy yasin <yasinsammy@gmail.com>
2023-04-24 00:04:59 -0500

    added wordlist and finalized logic of game

--- src/Hangman/HangmanGame.java
+++ src/Hangman/HangmanGame.java
@@ -1,5 +1,7 @@
 package Hangman;
 
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HangmanGame {
@@ -14,14 +16,18 @@ public class HangmanGame {
 
     private static int incorrectGuesses = 0;
 
+    private static int correctGuesses = 0;
+    private static HashSet<String> lettersGuessed;
+
 
 
     public HangmanGame(){
         board = new String[]{"  ┌──┐",
-                             "  O  │",
+                             "     │",
                              "     │",
                              "     │",
                              "   ──┴──"};
+        lettersGuessed = new HashSet<String>();
     }
 
     public void printBeginningBoard(){
@@ -61,57 +67,142 @@ public class HangmanGame {
         for (String toGuess : lettersToGuess) {
             System.out.print(toGuess);
         }
+        messageToPlayer();
     }
 
     public void playGame(){
-        word = "hello";
+
+        Scanner input = new Scanner(System.in);
+        word = getRandomWord(wordList);
+
+        System.out.println("Press 1 to play the game or Press 2 for testing purposes");
+        String playOrTest = input.next();
+
+        switch (playOrTest){
+            case "1":
+                System.out.println("Enjoy the game!");
+                break;
+            case "2":
+                System.out.println("The word is: " + word);
+                break;
+            default:
+                System.out.println("Input not recognized... oh well the games starting anyway");
+                break;
+        }
+
         lengthOfWord = word.length();
         lettersToGuess = new String[lengthOfWord];
         for (int i = 0; i < lengthOfWord; i++) {
             lettersToGuess[i] = " - ";
         }
-        Scanner input = new Scanner(System.in);
+        board = new String[]{"  ┌──┐",
+                             "     │",
+                             "     │",
+                             "     │",
+                             "   ──┴──"};
+
+        while(incorrectGuesses < 6 & correctGuesses < lengthOfWord){
+            printBoard();
+            printLetters();
+            String guess = input.next();
+            while(!verifySingleLetterGuess(guess)){
+                System.out.println("Your guess can only be one letter! Please enter another letter: ");
+                guess = input.next();
+            }
+            updateBoard(board, lettersToGuess, guess, word);
+        }
         printBoard();
         printLetters();
-        messageToPlayer();
+
+        input.nextLine();
+        System.out.println("Would you like to play again? (Y/N)");
+        String choice = input.nextLine();
+        while(!choice.toLowerCase().equals("y") & !choice.toLowerCase().equals("n")){
+            System.out.println("Please only select Y or N");
+            choice = input.next();
+        }
+        if(choice.toLowerCase().equals("y")){
+            resetStats();
+            startGame();
+
+        }
+        else printEndBoard();
 
     }
 
     public void messageToPlayer(){
-        if(incorrectGuesses < 6) System.out.println("Please guess a letter");
+        System.out.println("Letters Guessed: " + lettersGuessed);
+        if(incorrectGuesses == 6) System.out.println("Sorry you LOSE! The correct word was: " + word);
+        else if(correctGuesses == lengthOfWord) System.out.println("Congratulations you WON!");
+        else System.out.println("Please Guess a Letter: ");
     }
 
-    public void updateBoard(String[] board, String[] lettersToGuess, int incorrectGuesses, String guess, String word){
-        if(!word.contains(guess)){
+    public void updateBoard(String[] board, String[] lettersToGuess, String guess, String word){
+        if(!word.contains(guess) & !lettersGuessed.contains(guess)){
+            lettersGuessed.add(guess);
             incorrectGuesses++;
-            switch (incorrectGuesses) {
-                case 1:
+            switch (String.valueOf(incorrectGuesses)) {
+                case "1":
                     board[1] = "  O  │";
                     break;
-                case 2:
+                case "2":
                     board[2] = " /   │";
                     break;
-                case 3:
-                    board[3] = " /|  │";
+                case "3":
+                    board[2] = " /|  │";
                     break;
-                case 4:
-                    board[4] = " /|\\ │";
+                case "4":
+                    board[2] = " /|\\ │";
                     break;
-                case 5:
-                    board[5] = " /   │";
+                case "5":
+                    board[3] = " /   │";
                     break;
-                case 6:
-                    board[6] = " / \\ │";
+                case "6":
+                    board[3] = " / \\ │";
                     break;
                 default:
                     System.out.println("Game Over");
             }
         }
         else {
+            if(!lettersGuessed.contains(guess)){
             for (int i = 0; i < word.length(); i++) {
-                if(guess.equals("" + word.charAt(i))) lettersToGuess[i] = " " + guess + " ";
+                if(guess.equals("" + word.charAt(i))){
+                        lettersToGuess[i] = " " + guess + " ";
+                        lettersGuessed.add(guess);
+                        correctGuesses++;
+                }
             }
+        }
         }
 
     }
+
+    public void resetStats(){
+        incorrectGuesses = 0;
+        correctGuesses = 0;
+        lettersGuessed.clear();
+    }
+
+    public static Boolean verifySingleLetterGuess(String guess){
+        return guess.length() == 1 & Character.isLetter(guess.charAt(0));
+    }
+
+    public String getRandomWord(String[] words) {
+        Random random = new Random();
+        int index = random.nextInt(words.length);
+        return words[index];
+    }
+
+    private static String[] wordList = {"apple", "banana", "orange", "grape", "pineapple", "watermelon", "kiwi", "strawberry",
+            "peach", "apricot", "plum", "pear", "blueberry", "raspberry", "mango", "papaya", "lemon", "lime", "cherry", "coconut",
+            "melon", "fig", "guava", "kiwifruit", "nectarine", "pomegranate", "prune", "tangerine", "cranberry",
+            "blackberry", "elderberry","grapefruit", "mandarin", "cantaloupe", "honeydew", "dragonfruit", "starfruit",
+            "passionfruit", "lychee", "durian", "rhubarb", "tomato", "eggplant", "cucumber", "zucchini", "pumpkin", "potato", "carrot",
+            "kale", "broccoli", "cauliflower", "cabbage", "asparagus", "peas", "beans", "lentils", "chickpeas", "soybeans", "cashews",
+            "almonds", "walnuts", "peanuts", "hazelnuts", "macadamia", "pistachio", "sesame", "flaxseed", "chia", "sunflower", "pumpkinseed",
+            "oatmeal", "rice", "quinoa", "barley", "wheat", "amaranth", "oats", "flour", "yeast", "sugar", "honey", "maple" , "syrup",
+            "molasses", "agave", "nectar", "corn syrup", "rice","corn", "pepper", "onion", "garlic",
+            "ginger", "spinach", "lettuce"};
+
 }
--- src/Hangman/Requirements.java
+++ src/Hangman/Requirements.java
@@ -12,9 +12,8 @@ The game should have a simple graphical user interface that displays the followi
 Guessing Mechanism
 -The player should be able to guess letters by typing them on the keyboard.
 -The game should only allow single letter guesses
--If the player makes an invalid guess( any non-letter or blank guess) they will be shown the corresponding message
 -If the player guesses a correct letter, the corresponding blank should be filled in with the letter.
--If the player guesses an incorrect letter, the hangman should be incrementally drawn on the screen, and the number of remaining guesses should decrease by one.
+-If the player guesses an incorrect letter, the hangman should be incrementally drawn on the screen.
 -If the player runs out of guesses before guessing the word, the game should end, and the player should be shown the correct word.
 
 Win/Lose Conditions
@@ -23,16 +22,4 @@ Win/Lose Conditions
 -If the player runs out of guesses, they should be shown a message indicating that they lost and the correct word.
 -They should also be given the option to play again.
 
-     */
-    public static void main(String[] args) {
-        System.out.println("  O");
-        System.out.println(" /|\\");
-        System.out.println(" / \\");
-
-        System.out.println("  ┌──┐");
-        System.out.println("  O  │      Start game? (y/n)");
-        System.out.println(" /|\\ │");
-        System.out.println(" / \\ │");
-        System.out.println("   ──┴──");
-    }
-}
+     */}
